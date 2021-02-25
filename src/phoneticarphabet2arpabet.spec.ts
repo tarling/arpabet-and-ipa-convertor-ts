f844d1fcca8bb1fb4166f53f164ff120c005dfd1
James Tarling <james.tarling@ovoenergy.com>
2021-02-25 21:53:31 +0000

    Use modules instead of classes

--- src/phoneticarphabet2arpabet.spec.ts
+++ src/phoneticarphabet2arpabet.spec.ts
@@ -1,62 +1,60 @@
-import { PhoneticAlphabet2ARPAbet } from './phoneticarphabet2arpabet';
+import { convert } from './phoneticarphabet2arpabet';
 
 describe('PhoneticAlphabet2ARPAbet', () => {
-    const convertor = new PhoneticAlphabet2ARPAbet();
-
     it('Monosyllable and ()', () => {
         // 单音节及()
-        const result = convertor.convert('faʊ(hh)nd');
+        const result = convert('faʊ(hh)nd');
         expect(result).toEqual('F AW1 N D');
     });
 
     it('Double syllable', () => {
          // ri'trai 双音节
-        const result = convertor.convert('riˈtraɪ');
+        const result = convert('riˈtraɪ');
         expect(result).toEqual('R IY0 T R AY1');
     });
 
     it('Trisyllable', () => {
         // ˈɛniˌwʌn, -wən 三音节
-        const result = convertor.convert('ˈɛniˌwʌn');
+        const result = convert('ˈɛniˌwʌn');
         expect(result).toEqual('EH1 N IY0 W AH2 N');
     });
 
     it('kʊd', () => {
-        const result = convertor.convert('kʊd');
+        const result = convert('kʊd');
         expect(result).toEqual('K UH1 D');
     });
 
     it('wilkinsn Double syllable', () => {
         // wilkinsn 双音节
-        const result = convertor.convert("'wilkinsn");
+        const result = convert("'wilkinsn");
         expect(result).toEqual('W IY1 L K IY0 N S N');
     });
 
     it('Edge test wrong position of accent mark', () => {
         // 边缘测试 重音标识位置不对
         expect(() => {
-            convertor.convert("wilkins'n");
+            convert("wilkins'n");
         }).toThrow('ns\' The accent mark is inappropriate, and the previous syllable of \' has no vowels!'); // ns\' 重音标识不合适，\'前一个音节没有元音！
     });
 
     it('Edge test Wrong accent mark and no vowel', () => {
         // 边缘测试 重音标识不对，并没有元音
         expect(() => {
-            convertor.convert("wilkia'ʊsn");
+            convert("wilkia'ʊsn");
         }).toThrow('There is an unrecognized phonetic transcription a'); // 存在不能识别的音标 a
     });
 
     it('Edge test Find the completeness of the last syllable', () => {
         // 边缘测试 最后一个音节美音找完整
         expect(() => {
-            convertor.convert("wilki'sna");
+            convert("wilki'sna");
         }).toThrow('There is an unrecognized phonetic transcription a'); // 存在不能识别的音标 a
     });
 
     it('Edge test. An incomplete syllable in the middle is connected to a proper phonetic symbol.', () => {
         // 边缘测试 中间一个不完整的音节连带到做好匹配不到合适的音标
         expect(() => {
-            convertor.convert("wilki'san");
+            convert("wilki'san");
         }).toThrow('There is an unrecognized phonetic transcription an'); // 存在不能识别的音标 an
     });
 });
--- src/phoneticarphabet2arpabet.ts
+++ src/phoneticarphabet2arpabet.ts
@@ -5,12 +5,6 @@ import { Stress } from './model/stress';
 import { Syllable } from './model/syllable';
 import { Word } from './model/word';
 
-enum ConvertPriority {
-    American,
-    English,
-    IPA,
-}
-
 /*
 Stress and auxiliary symbols[3]
 AB	Description
@@ -27,162 +21,162 @@ AB	Description
 :1 or .	Falling or declining juncture
 :2 or ?	Rising or internal juncture
 :3 or .	Fall-rise or non-term juncture*/
-const primaryStressIpa = ["'", "\u02c8"];
-const secondaryStressIpa = ["\u02cc"];
-const ipaStops = [",", "-"];
-const skip = [
-    ["(", ")"],
-    ["\uff08", "\uff09"],
-];
+
+
+enum ConvertPriority {
+  American,
+  English,
+  IPA,
+}
 
 type PhonemeTree = Record<string, Phoneme>;
 
-export class PhoneticAlphabet2ARPAbet {
-    private ipaTree: PhonemeTree = {};
-    private kkTree: PhonemeTree = {};
-    private djTree: PhonemeTree = {};
+const ipaTree: PhonemeTree = {};
+const kkTree: PhonemeTree = {};
+const djTree: PhonemeTree = {};
 
-    private stopsDict: Record<string, boolean> = {};
-    private skipDict: Record<string, string> = {};
-    private stressDict: Record<string, Stress> = {};
+vowels.forEach(o => {
+    ipaTree[o.ipa] = o;
+    kkTree[o.american] = o;
+    djTree[o.english] = o;
+});
+consonants.forEach(o => {
+    ipaTree[o.ipa] = o;
+    kkTree[o.american] = o;
+    djTree[o.english] = o;
+});
 
-    constructor() {
-        /*
-        装配数据
-        */
-        vowels.forEach(o => {
-            this.ipaTree[o.ipa] = o;
-            this.kkTree[o.american] = o;
-            this.djTree[o.english] = o;
-        });
-        consonants.forEach(o => {
-            this.ipaTree[o.ipa] = o;
-            this.kkTree[o.american] = o;
-            this.djTree[o.english] = o;
-        });
+const skipDict: Record<string, string> = {};
+const stressDict: Record<string, Stress> = {};
 
-        ipaStops.forEach(str => {
-            this.stopsDict[str] = true;
-        });
 
-        skip.forEach(skip => {
-            this.skipDict[skip[0]] = skip[1];
-        });
+const skip = [
+  ["(", ")"],
+  ["\uff08", "\uff09"],
+];
+skip.forEach(skip => {
+    skipDict[skip[0]] = skip[1];
+});
 
-        primaryStressIpa.forEach(str => {
-            this.stressDict[str] = Stress.Primary;
-        });
+const primaryStressIpa = ["'", "\u02c8"];
+primaryStressIpa.forEach(str => {
+    stressDict[str] = Stress.Primary;
+});
 
-        secondaryStressIpa.forEach(str => {
-            this.stressDict[str] = Stress.Secondary;
-        });
-    }
-    private isStop(c:string): boolean {
-        return this.stopsDict[c] === true;
-    }
-    private findPhonemeInArphabetList(phoneme_string:string, arphabets: PhonemeTree[]): Phoneme | null {
-        for (const arphabet of arphabets) {
-            const phoneme = arphabet[phoneme_string];
-            if (phoneme) {
-                return phoneme;
-            }
+const secondaryStressIpa = ["\u02cc"];
+secondaryStressIpa.forEach(str => {
+    stressDict[str] = Stress.Secondary;
+});
+
+const ipaStops = [",", "-"];
+function isStop(c:string): boolean {
+    return ipaStops.includes(c);
+}
+
+function findPhonemeInArphabetList(phonemeString:string, arphabets: PhonemeTree[]): Phoneme | null {
+    for (const arphabet of arphabets) {
+        const phoneme = arphabet[phonemeString];
+        if (phoneme) {
+            return phoneme;
         }
+    }
+    return null;
+}
+
+function createArphabetsList(priority: ConvertPriority): PhonemeTree[] {
+    if ((ConvertPriority.American === priority)) {
+        return [kkTree, ipaTree, djTree];
+    }
+    if ((ConvertPriority.English === priority)) {
+        return [djTree, ipaTree, kkTree];
+    }
+    if ((ConvertPriority.IPA === priority)) {
+        return [ipaTree, kkTree, djTree];
+    }
+    throw new Error('Priority not recognised');
+}
+
+export function convert(arphabet: string, priority = ConvertPriority.American): string | null {
+    if ((! arphabet)) {
         return null;
     }
-    private createArphabetsList(priority: ConvertPriority): PhonemeTree[] {
-        if ((ConvertPriority.American === priority)) {
-            return [this.kkTree, this.ipaTree, this.djTree];
+    let temp_ch = "";
+    const skipStack: string[] = [];
+    const arphabet_list = createArphabetsList(priority);
+    let lastPhoneme: Phoneme | null = null;
+    const word = new Word();
+    let syllable = new Syllable();
+    let tempSyllableStr = "";
+    const chars = arphabet.split('');
+    chars.forEach((ch, index) => {
+        if (isStop(ch)) {
+            return;
         }
-        if ((ConvertPriority.English === priority)) {
-            return [this.djTree, this.ipaTree, this.kkTree];
-        }
-        if ((ConvertPriority.IPA === priority)) {
-            return [this.ipaTree, this.kkTree, this.djTree];
-        }
-        throw new Error('Priority not recognised');
-    }
-    convert(arphabet: string, priority = ConvertPriority.American): string | null {
-        if ((! arphabet)) {
-            return null;
-        }
-        let temp_ch = "";
-        const skipStack: string[] = [];
-        const arphabet_list = this.createArphabetsList(priority);
-        let lastPhoneme: Phoneme | null = null;
-        const word = new Word();
-        let syllable = new Syllable();
-        let tempSyllableStr = "";
-        const chars = arphabet.split('');
-        chars.forEach((ch, index) => {
-            if (this.isStop(ch)) {
+        tempSyllableStr += ch;
+        if (skipStack.length) {
+            if ((ch === skipDict[skipStack.slice((- 1))[0]])) {
+                skipStack.pop();
+            }
+            return;
+        } else {
+            if (skipDict[ch]) {
+                skipStack.push(ch);
                 return;
             }
-            tempSyllableStr += ch;
-            if (skipStack.length) {
-                if ((ch === this.skipDict[skipStack.slice((- 1))[0]])) {
-                    skipStack.pop();
-                }
-                return;
+        }
+        const stress = stressDict[ch];
+        if (stress) {
+            if (((! lastPhoneme) && (index > 0))) {
+                // 存在不能识别的音标 ${temp_ch}
+                throw new PhonemeError(`There is an unrecognized phonetic transcription ${temp_ch}`);
             } else {
-                if (this.skipDict[ch]) {
-                    skipStack.push(ch);
-                    return;
-                }
-            }
-            const stress = this.stressDict[ch];
-            if (stress) {
-                if (((! lastPhoneme) && (index > 0))) {
-                    // 存在不能识别的音标 ${temp_ch}
-                    throw new PhonemeError(`There is an unrecognized phonetic transcription ${temp_ch}`);
-                } else {
-                    /*
-                    遇到重音标识，说明前面是是一个音节，添加到word中，并清空last_phoneme及temp_ch
-                    */
-                    if (lastPhoneme) {
-                        syllable.addPhoneme(lastPhoneme);
-                        if ((! syllable.haveVowel)) {
-                            // ${tempSyllableStr} 重音标识不合适，${ch}前一个音节没有元音！
-                            throw new PhonemeError(`${tempSyllableStr} The accent mark is inappropriate, and the previous syllable of ${ch} has no vowels!`);
-                        }
-                        word.addSyllable(syllable);
-                        syllable = new Syllable();
-                    }
-                    lastPhoneme = null;
-                    temp_ch = "";
-                    tempSyllableStr = ch;
-                    syllable.stress = stress;
-                    return;
-                }
-            }
-            temp_ch += ch;
-            const tempPhoneme = this.findPhonemeInArphabetList(temp_ch, arphabet_list);
-            if ((lastPhoneme && (! tempPhoneme))) {
                 /*
-                说明前面是是一个完整音标
+                遇到重音标识，说明前面是是一个音节，添加到word中，并清空last_phoneme及temp_ch
                 */
-                syllable.addPhoneme(lastPhoneme);
-                if (lastPhoneme.isVowel) {
+                if (lastPhoneme) {
+                    syllable.addPhoneme(lastPhoneme);
+                    if ((! syllable.haveVowel)) {
+                        // ${tempSyllableStr} 重音标识不合适，${ch}前一个音节没有元音！
+                        throw new PhonemeError(`${tempSyllableStr} The accent mark is inappropriate, and the previous syllable of ${ch} has no vowels!`);
+                    }
                     word.addSyllable(syllable);
                     syllable = new Syllable();
-                    tempSyllableStr = ch;
                 }
-                temp_ch = ch;
-                lastPhoneme = this.findPhonemeInArphabetList(temp_ch, arphabet_list);
-            } else {
-                lastPhoneme = tempPhoneme;
+                lastPhoneme = null;
+                temp_ch = "";
+                tempSyllableStr = ch;
+                syllable.stress = stress;
+                return;
             }
-        });
-        if (lastPhoneme) {
-            syllable.addPhoneme(lastPhoneme);
-            if ((syllable.stress && (! syllable.haveVowel))) {
-                // ${tempSyllableStr} 有重音标识但并没有元音！
-                throw new PhonemeError(`${tempSyllableStr} has accent marks but no vowels!`);
-            }
-            word.addSyllable(syllable);
-        } else {
-            // 存在不能识别的音标 ${temp_ch}
-            throw new PhonemeError(`There is an unrecognized phonetic transcription ${temp_ch}`);
         }
-        return word.toArpabet();
+        temp_ch += ch;
+        const tempPhoneme = findPhonemeInArphabetList(temp_ch, arphabet_list);
+        if ((lastPhoneme && (! tempPhoneme))) {
+            /*
+            说明前面是是一个完整音标
+            */
+            syllable.addPhoneme(lastPhoneme);
+            if (lastPhoneme.isVowel) {
+                word.addSyllable(syllable);
+                syllable = new Syllable();
+                tempSyllableStr = ch;
+            }
+            temp_ch = ch;
+            lastPhoneme = findPhonemeInArphabetList(temp_ch, arphabet_list);
+        } else {
+            lastPhoneme = tempPhoneme;
+        }
+    });
+    if (lastPhoneme) {
+        syllable.addPhoneme(lastPhoneme);
+        if ((syllable.stress && (! syllable.haveVowel))) {
+            // ${tempSyllableStr} 有重音标识但并没有元音！
+            throw new PhonemeError(`${tempSyllableStr} has accent marks but no vowels!`);
+        }
+        word.addSyllable(syllable);
+    } else {
+        // 存在不能识别的音标 ${temp_ch}
+        throw new PhonemeError(`There is an unrecognized phonetic transcription ${temp_ch}`);
     }
+    return word.toArpabet();
 }
